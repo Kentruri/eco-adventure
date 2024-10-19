50867c64f53356211818a889d4e0052cf83949d6
nicolasgranada <144197255+nicolasgranada@users.noreply.github.com>
2024-10-19 08:42:16 -0500

    Adding my unit test

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+  return <button onClick={onClick}>{label}</button>;
+};
